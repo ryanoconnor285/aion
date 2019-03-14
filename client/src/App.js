8d444e360b0723b2a5319fb7852bedac1a11a34b
rocsteady888 <rocsteady888@gmail.com>
2019-03-14 19:00:44 -0400

    added routers, redux and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import PrivateRoute from './components/common/PrivateRoute';
+
 import AppBar from './components/layout/navigation/AppBar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Landing from './components/layout/content/Landing';
+import Dashboard from './components/layout/content/Dashboard';
+
 import './App.css';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppBar />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <AppBar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
