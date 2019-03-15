a72d0d08ef36c53c91a1beb9c0ca057375946071
rocsteady888 <rocsteady888@gmail.com>
2019-03-15 15:23:09 -0400

    added a loading state to workShifts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,5 @@
 export const GET_ERRORS = 'GET_ERRORS';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const GET_SHIFTS = "GET_SHIFTS"
+export const GET_WORK_SHIFTS = "GET_WORK_SHIFTS"
+export const WORK_SHIFT_LOADING = "WORK_SHIFT_LOADING"
