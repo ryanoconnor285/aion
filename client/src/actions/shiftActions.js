106a72f55f13cda7fe0be1698db1eb9085084338
rocsteady888 <rocsteady888@gmail.com>
2019-03-16 14:45:00 -0400

    added clock in action

--- client/src/actions/shiftActions.js
+++ client/src/actions/shiftActions.js
@@ -1,6 +1,44 @@
 import axios from 'axios';
 
-import { GET_WORK_SHIFTS, WORK_SHIFT_LOADING } from './types';
+import { GET_WORK_SHIFTS, WORK_SHIFT_LOADING, CLOCK_IN, CLOCK_OUT, GET_ERRORS, CLEAR_ERRORS } from './types';
+
+// Clock In
+export const clockIn = newWorkShift => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post('api/shift/clockIn', newWorkShift)
+    .then(res =>
+      dispatch({
+        type: CLOCK_IN,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Clock Out
+export const clockOut = clockOutData => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post(`api/shift/clockOut/${clockOutData.id}`, clockOutData)
+    .then(res =>
+      dispatch({
+        type: CLOCK_OUT,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
 
 // Get all of current user's shifts
 export const getShifts = () => dispatch => {
@@ -24,5 +62,12 @@ export const getShifts = () => dispatch => {
 export const setShiftLoading = () => {
   return {
     type: WORK_SHIFT_LOADING
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   };
 };
