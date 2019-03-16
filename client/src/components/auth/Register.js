46d228ac61ebe9bd4f3882665d3d2ff020838d86
rocsteady888 <rocsteady888@gmail.com>
2019-03-15 21:51:54 -0400

    intial commit

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,124 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
 
-class Register extends React.Component {
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      errors: {}
+    };
+
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password,
+      confirmPassword: this.state.confirmPassword
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  }
+
   render() {
+    const { errors } = this.state;
+
     return (
-      <div>
-        <h1>Register component</h1>
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form noValidate onSubmit={this.onSubmit}>
+                <input
+                  placeholder="First Name"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.onChange}
+                />
+                <span>{errors.firstName ? errors.firstName : null}</span>
+                <input
+                  placeholder="Last Name"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.onChange}
+                />
+                <span>{errors.lastName ? errors.lastName : null}</span>
+                <input
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                />
+                <span>{errors.email ? errors.email : null}</span>
+                <input
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                />
+                <span>{errors.password ? errors.password : null}</span>
+                <input
+                  placeholder="Confirm Password"
+                  name="confirmPassword"
+                  type="password"
+                  value={this.state.confirmPassword}
+                  onChange={this.onChange}
+                  error={errors.confirmPassword}
+                />
+                <span>{errors.confirmPassword ? errors.confirmPassword : null}</span>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { registerUser })(withRouter(Register));
