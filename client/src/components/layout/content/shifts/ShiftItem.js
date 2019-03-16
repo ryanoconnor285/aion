2d4d66b2988ac9271ac26a41c711c9ed9ab2e634
rocsteady888 <rocsteady888@gmail.com>
2019-03-16 14:46:20 -0400

    dummy component to render workshift data

--- client/src/components/layout/content/shifts/ShiftItem.js
+++ client/src/components/layout/content/shifts/ShiftItem.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Button from '@material-ui/core/Button';
 
 class ShiftItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clockOutDesc: '',
+    }
+  }
+
+  handleClockOut = (e) => {
+    e.preventDefault();
+
+    const clockOutData = {
+      clockOutDesc: this.state.clockOutDesc,
+      id: e.target.id
+    };
+    console.log(clockOutData);
+    this.props.clockOut(clockOutData);
+    this.setState({ clockInDesc: '' });
+  }
   render() {
     const { workShifts } = this.props;
     return(
       <div>
-        <h1>Shift Item</h1>
-        <h3>{workShifts.user}</h3>
-        <h3>{workShifts.clockIn}</h3>
-        <h3>{workShifts.clockInDesc}</h3>
-        <h3>{workShifts.clockOut}</h3>
-        <h3>{workShifts.clockOutDesc}</h3>
+        <h3>Shift Item</h3>
+        <h5>{workShifts.user}</h5>
+        <h5>{workShifts.clockIn}</h5>
+        <h5>{workShifts.clockInDesc}</h5>
+        <h5>{workShifts.clockOut ? workShifts.clockOut : <Button onClick={this.handleClockOut}>Clock Out</Button>}</h5>
+        <h5>{workShifts.clockOutDesc}</h5>
       </div>
     )
   }
