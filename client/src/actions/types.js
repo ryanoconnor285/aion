2e360fe88e3ed424af8525ae5453d48ddd989973
rocsteady888 <rocsteady888@gmail.com>
2019-03-16 14:45:21 -0400

    added clock in and clock out types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 export const GET_ERRORS = 'GET_ERRORS';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const GET_WORK_SHIFTS = "GET_WORK_SHIFTS"
-export const WORK_SHIFT_LOADING = "WORK_SHIFT_LOADING"
+export const GET_WORK_SHIFTS = "GET_WORK_SHIFTS";
+export const WORK_SHIFT_LOADING = "WORK_SHIFT_LOADING";
+export const CLOCK_IN = "CLOCK_IN";
+export const CLOCK_OUT = "CLOCK_OUT";
