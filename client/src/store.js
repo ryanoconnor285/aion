2b0dc600130cb9cefdecb8edf19fb2fa5c815e8e
rocsteady888 <rocsteady888@gmail.com>
2019-03-16 21:53:39 -0400

    fixed an issue with REDUX tools preventing app from loading on mobile

--- client/src/store.js
+++ client/src/store.js
@@ -5,14 +5,16 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleware = [thunk];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-  )
+  enhancer
 );
 
 export default store;
