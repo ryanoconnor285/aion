b1c9475e08f651e31576d7adcb9889a0d3e74011
rocsteady888 <rocsteady888@gmail.com>
2019-03-16 20:41:52 -0400

    added seed URI

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI: 'mongodb://ryan:abc123@ds127825.mlab.com:27825/kronos',
+  seedURI: 'mongoimport -h ds127825.mlab.com:27825 -d kronos -c shifts -u ryan -p abc123 --file ./seed/seed.json',
   secretOrKey: 'secret'
 };
