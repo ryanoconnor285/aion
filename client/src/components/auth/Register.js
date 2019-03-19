fe79c1c487cb24772a30fcbc5c5c35ecf886f770
rocsteady888 <rocsteady888@gmail.com>
2019-03-19 18:35:05 -0400

    makes registration form mobile friendly

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../../css/register.css';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -10,6 +11,7 @@ class Register extends Component {
     this.state = {
       firstName: '',
       lastName: '',
+      timeStampAction: '',
       email: '',
       password: '',
       confirmPassword: '',
@@ -40,6 +42,7 @@ class Register extends Component {
     const newUser = {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
+      timeStampAction: this.state.timeStampAction,
       email: this.state.email,
       password: this.state.password,
       confirmPassword: this.state.confirmPassword
@@ -53,58 +56,70 @@ class Register extends Component {
 
     return (
       <div className="register">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-8 m-auto">
-              <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">
-                Create your DevConnector account
-              </p>
-              <form noValidate onSubmit={this.onSubmit}>
-                <input
-                  placeholder="First Name"
-                  name="firstName"
-                  value={this.state.firstName}
-                  onChange={this.onChange}
-                />
-                <span>{errors.firstName ? errors.firstName : null}</span>
-                <input
-                  placeholder="Last Name"
-                  name="lastName"
-                  value={this.state.lastName}
-                  onChange={this.onChange}
-                />
-                <span>{errors.lastName ? errors.lastName : null}</span>
-                <input
-                  placeholder="Email"
-                  name="email"
-                  type="email"
-                  value={this.state.email}
-                  onChange={this.onChange}
-                />
-                <span>{errors.email ? errors.email : null}</span>
-                <input
-                  placeholder="Password"
-                  name="password"
-                  type="password"
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-                <span>{errors.password ? errors.password : null}</span>
-                <input
-                  placeholder="Confirm Password"
-                  name="confirmPassword"
-                  type="password"
-                  value={this.state.confirmPassword}
-                  onChange={this.onChange}
-                  error={errors.confirmPassword}
-                />
-                <span>{errors.confirmPassword ? errors.confirmPassword : null}</span>
-                <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
-            </div>
-          </div>
-        </div>
+        <h1 className="register-header">Sign Up</h1>
+        <form className="register-container" noValidate onSubmit={this.onSubmit}>
+          <p>
+            <input
+              placeholder="First Name"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.onChange}
+            />
+          </p>
+          <span>{errors.firstName ? errors.firstName : null}</span>
+          <p>
+            <input
+              placeholder="Last Name"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.onChange}
+            />
+          </p>
+          <span>{errors.lastName ? errors.lastName : null}</span>
+          <p>
+            <input
+              placeholder="Time Stamp Action"
+              name="timeStampAction"
+              type="text"
+              value={this.state.timeStampAction}
+              onChange={this.onChange}
+            />
+          </p>
+          <small>If you clock in by calling a phone number enter it here, otherwise leave it blank</small>
+          <p>
+            <input
+              placeholder="Email"
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.onChange}
+            />
+          </p>
+          <span>{errors.email ? errors.email : null}</span>
+          <p>
+            <input
+              placeholder="Password"
+              name="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+          </p>
+          <span>{errors.password ? errors.password : null}</span>
+          <p>
+            <input
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              type="password"
+              value={this.state.confirmPassword}
+              onChange={this.onChange}
+              error={errors.confirmPassword}
+            />
+          </p>
+          <span>{errors.confirmPassword ? errors.confirmPassword : null}</span>
+          <span>{errors.emailExists ? errors.emailExists : null}</span>
+          <input type="submit" className="sumbit-btn" value="Register"/>
+        </form>
       </div>
     );
   }
