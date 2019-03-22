fe955edb5cc58a2669ec6a80ca4997c91a71f7c2
rocsteady888 <rocsteady888@gmail.com>
2019-03-22 19:30:41 -0400

    css changed to style becaues it holds themes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { loginUser } from '../../actions/authActions';
-import '../../css/login.css'
+import '../../style/login.css'
 
 class Login extends Component {
   constructor() {
