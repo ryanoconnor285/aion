bea68f035cb96e39ef929e4ac40a988f398c9d82
rocsteady888 <rocsteady888@gmail.com>
2019-03-23 19:56:59 -0400

    temproary theme

--- client/src/style/Theme.js
+++ client/src/style/Theme.js
@@ -1,13 +1,23 @@
 
 
 import { createMuiTheme } from '@material-ui/core/styles';
-import { deepOrange, blue } from '@material-ui/core/colors';
+
 
 
 const theme = createMuiTheme({
   palette: {
-    primary: blue,
-    secondary: deepOrange,
+    primary: {
+      light: '#0091ea',
+      main: '#01579b',
+      dark: '#0d47a1',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#ff5722',
+      dark: '#e64a19',
+      contrastText: '#000',
+    },
   },
   status: {
     danger: 'orange',
