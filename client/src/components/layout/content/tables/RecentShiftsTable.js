f1f27118a3fe9e200965195bc4ce70c908f68d86
rocsteady888 <rocsteady888@gmail.com>
2019-03-26 11:52:28 -0400

    fixed issue with map fuction crashing

--- client/src/components/layout/content/tables/RecentShiftsTable.js
+++ client/src/components/layout/content/tables/RecentShiftsTable.js
@@ -3,6 +3,8 @@ import ClockOutModal from '../shifts/ClockOutModal';
 import EditShiftModal from '../shifts/EditShiftModal';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import isEmpty from '../../../../validation/isEmpty';
+import CircularProgress from '../../../common/CircularProgress';
 import { getOpenShifts, getShifts } from '../../../../actions/shiftActions';
 import Moment from 'react-moment';
 import moment from 'moment';
@@ -35,70 +37,83 @@ class RecentShifts extends React.Component {
       clockOutDesc: '',
     }
   }
-
-  componentDidMount(){
-    // this.props.getShifts();
+  
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+    if (nextProps.workShift) {
+      this.setState({ workShift: nextProps.workShift });
+    }
   }
 
   render() {
-    const { workShifts, classes } = this.props;
+    const { workShifts, loading, classes } = this.props;
     const dateFormat = "MM/DD HH:mm";
-    const workShiftTable = workShifts.map(workshift =>
-      <TableRow className={classes.tableRow} key={workshift._id} id={workshift._id}>
-        <TableCell>
-          <Moment format={dateFormat}>
-            {workshift.clockIn}
-          </Moment>
-        </TableCell>
-        <TableCell>
-          {workshift.clockInDesc}
-        </TableCell>
-        <TableCell>
-          {
-            workshift.clockOut 
-            ?
-            <Moment format={dateFormat}>{workshift.clockOut}</Moment>
-            :
-            <ClockOutModal btnText={"Clock Out"} id={workshift._id} />
-          }
-        </TableCell>
-        <TableCell>
-          {workshift.clockOutDesc}
-        </TableCell>
-        <TableCell>
-          {
-            workshift.clockOut 
-            ?
-            moment(workshift.clockOut).diff(moment(workshift.clockIn), 'hours', true).toFixed(1) + ' hrs'
-            :
-            workshift.clockOut
-          }
-        </TableCell>
-        <TableCell>
-          <EditShiftModal 
-            id={workshift._id}
-            workshift={workshift}
-          />
-        </TableCell>
-      </TableRow>
-    )
+    let workShiftTable;
+    if (loading) {
+      workShiftTable = <CircularProgress />;
+    } else if (isEmpty(workShifts)) {
+      workShiftTable = <span>No shifts to display yet</span>
+    } else {
+      workShiftTable = 
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Clock In</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Clock Out</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Duration</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {workShifts.map(workshift =>
+            <TableRow className={classes.tableRow} key={workshift._id} id={workshift._id}>
+              <TableCell>
+                <Moment format={dateFormat}>
+                  {workshift.clockIn}
+                </Moment>
+              </TableCell>
+              <TableCell>
+                {workshift.clockInDesc}
+              </TableCell>
+              <TableCell>
+                {
+                  workshift.clockOut
+                  ?
+                  <Moment format={dateFormat}>{workshift.clockOut}</Moment>
+                  :
+                  <ClockOutModal btnText={"Clock Out"} id={workshift._id} />
+                }
+              </TableCell>
+              <TableCell>
+                {workshift.clockOutDesc}
+              </TableCell>
+              <TableCell>
+                {
+                  workshift.clockOut
+                  ?
+                  moment(workshift.clockOut).diff(moment(workshift.clockIn), 'hours', true).toFixed(1) + ' hrs'
+                  :
+                  workshift.clockOut
+                }
+              </TableCell>
+              <TableCell>
+                <EditShiftModal
+                  id={workshift._id}
+                  workshift={workshift}
+                />
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    }
 
     return (
       <Paper className={classes.tableContainer}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Clock In</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Clock Out</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Duration</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {workShiftTable}
-          </TableBody>
-        </Table>
+        {workShiftTable}
       </Paper>
     )
   }
