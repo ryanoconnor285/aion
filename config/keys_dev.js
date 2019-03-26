c4f0ca9a9363c2245bd45b7542b9621420f1f521
rocsteady888 <rocsteady888@gmail.com>
2019-03-26 17:27:21 -0400

    seed file removed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,4 @@
 module.exports = {
   mongoURI: 'mongodb://ryan:abc123@ds127825.mlab.com:27825/kronos',
-  seedURI: 'mongoimport -h ds127825.mlab.com:27825 -d kronos -c shifts -u ryan -p abc123 --file ./seed/seed.json',
   secretOrKey: 'secret'
 };
