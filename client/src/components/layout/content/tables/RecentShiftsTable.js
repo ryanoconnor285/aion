3e55f3acccfa25f05fdaacb2ac328b94296ee05a
rocsteady888 <rocsteady888@gmail.com>
2019-03-26 21:06:07 -0400

    added grid for a container

--- client/src/components/layout/content/tables/RecentShiftsTable.js
+++ client/src/components/layout/content/tables/RecentShiftsTable.js
@@ -9,7 +9,7 @@ import { getOpenShifts, getShifts } from '../../../../actions/shiftActions';
 import Moment from 'react-moment';
 import moment from 'moment';
 import { withStyles } from '@material-ui/core/styles';
-import { Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import { Paper, Grid, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 
 
 const styles = theme => ({
@@ -57,58 +57,60 @@ class RecentShifts extends React.Component {
       workShiftTable = <span>No shifts to display yet</span>
     } else {
       workShiftTable = 
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Clock In</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Clock Out</TableCell>
-            <TableCell>Description</TableCell>
-            <TableCell>Duration</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {workShifts.map(workshift =>
-            <TableRow className={classes.tableRow} key={workshift._id} id={workshift._id}>
-              <TableCell>
-                <Moment format={dateFormat}>
-                  {workshift.clockIn}
-                </Moment>
-              </TableCell>
-              <TableCell>
-                {workshift.clockInDesc}
-              </TableCell>
-              <TableCell>
-                {
-                  workshift.clockOut
-                  ?
-                  <Moment format={dateFormat}>{workshift.clockOut}</Moment>
-                  :
-                  <ClockOutModal btnText={"Clock Out"} id={workshift._id} />
-                }
-              </TableCell>
-              <TableCell>
-                {workshift.clockOutDesc}
-              </TableCell>
-              <TableCell>
-                {
-                  workshift.clockOut
-                  ?
-                  moment(workshift.clockOut).diff(moment(workshift.clockIn), 'hours', true).toFixed(1) + ' hrs'
-                  :
-                  workshift.clockOut
-                }
-              </TableCell>
-              <TableCell>
-                <EditShiftModal
-                  id={workshift._id}
-                  workshift={workshift}
-                />
-              </TableCell>
+      <Grid sm={false}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Clock In</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Clock Out</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Duration</TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {workShifts.map(workshift =>
+              <TableRow className={classes.tableRow} key={workshift._id} id={workshift._id}>
+                <TableCell>
+                  <Moment format={dateFormat}>
+                    {workshift.clockIn}
+                  </Moment>
+                </TableCell>
+                <TableCell>
+                  {workshift.clockInDesc}
+                </TableCell>
+                <TableCell>
+                  {
+                    workshift.clockOut
+                    ?
+                    <Moment format={dateFormat}>{workshift.clockOut}</Moment>
+                    :
+                    <ClockOutModal btnText={"Clock Out"} id={workshift._id} />
+                  }
+                </TableCell>
+                <TableCell>
+                  {workshift.clockOutDesc}
+                </TableCell>
+                <TableCell>
+                  {
+                    workshift.clockOut
+                    ?
+                    moment(workshift.clockOut).diff(moment(workshift.clockIn), 'hours', true).toFixed(1) + ' hrs'
+                    :
+                    workshift.clockOut
+                  }
+                </TableCell>
+                <TableCell>
+                  <EditShiftModal
+                    id={workshift._id}
+                    workshift={workshift}
+                  />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </Grid>
     }
 
     return (
