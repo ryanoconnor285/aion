5145694c376512cd295de917b94b45bdfc7af0e5
rocsteady888 <rocsteady888@gmail.com>
2019-03-26 21:02:19 -0400

    added notifications for openShifts.  Conditional rendering of table/card

--- client/src/components/layout/content/ShiftInfoTabs.js
+++ client/src/components/layout/content/ShiftInfoTabs.js
@@ -2,11 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import RecentShiftsTable from './tables/RecentShiftsTable';
+import ShiftCard from '../content/cards/ShiftCard';
 import OpenShifts from './shifts/OpenShifts';
+import isEmpty from '../../../validation/isEmpty';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
+import Badge from '@material-ui/core/Badge';
 
 function TabContainer(props) {
   return (
@@ -25,6 +28,9 @@ const styles = theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
   },
+  padding: {
+    padding: `0 ${theme.spacing.unit * 2}px`,
+  },
 });
 
 class ShiftInfoTabs extends React.Component {
@@ -38,18 +44,39 @@ class ShiftInfoTabs extends React.Component {
 
   render() {
     const { workShifts, openShifts, classes } = this.props;
-    const { value } = this.state;
-
+    const { value } = this.state; 
+    const isMobile = window.innerWidth <= 500;
+    let openShiftCount; 
+    if (isEmpty(openShifts)) {
+      openShiftCount = null
+    } else {
+      openShiftCount = openShifts.length
+    }
     return (
       <div className={classes.root}>
         <AppBar position="static">
           <Tabs value={value} onChange={this.handleChange} variant="fullWidth">
             <Tab label="Recent Shifts" />
-            <Tab label="Open Shifts" />
+            <Tab
+              label={
+                <Badge className={classes.padding} color="secondary" badgeContent={openShiftCount}>
+                  Open Shifts
+                </Badge>
+              }
+            />
             <Tab label="Current Pay Period" />
           </Tabs>
         </AppBar>
-        {value === 0 && <TabContainer><RecentShiftsTable workShifts={workShifts} /></TabContainer>}
+        {value === 0 && 
+          <TabContainer>
+            {
+              isMobile 
+              ?
+              <ShiftCard />
+              :
+              <RecentShiftsTable workShifts={workShifts} /> 
+            }
+          </TabContainer>}
         {value === 1 && <TabContainer><OpenShifts openShifts={openShifts}/></TabContainer>}
         {value === 2 && <TabContainer>Coming Soon</TabContainer>}
       </div>
