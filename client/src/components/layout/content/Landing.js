4cdab57cb2738f881ec5bc179f86c10b1babddf3
Ryan O'Connor <rocsteady888@gmail.com>
2019-03-28 13:22:40 -0400

    styling

--- client/src/components/layout/content/Landing.js
+++ client/src/components/layout/content/Landing.js
@@ -3,25 +3,30 @@ import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import mainImage from '../../../images/abstract-astronomy-dark-924824.jpg';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const styles = {
+const styles = theme => ({
   card: {
-    width: 375,
+    width: '100%',
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+  media: {
+    width: '100%',
+    height: '90%',
   },
-  title: {
-    fontSize: 14,
+  padding: {
+    [theme.breakpoints.down('xs')]: {
+      margin: theme.spacing.unit * 2,
+    },
+    [theme.breakpoints.up('md')]: {
+      padding: `0 ${theme.spacing.unit * 2}px`,
+    },
   },
-  pos: {
-    marginBottom: 12,
-  },
-};
+});
 
 class Landing extends React.Component {
   componentDidMount() {
@@ -31,28 +36,29 @@ class Landing extends React.Component {
   }
   render() {
     const { classes } = this.props;
-    const bull = <span className={classes.bullet}>•</span>;
 
     return (
       <Card className={classes.card}>
-        <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Aion (Greek: Αἰών)
-        </Typography>
-          <Typography variant="h5" component="h2">
-            A
-          {bull}
-            ion
-        </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            (deity)
-        </Typography>
-          <Typography component="p">
-             is a Hellenistic deity associated with time, the orb or circle encompassing the universe, and the zodiac. The "time" represented by Aion is unbounded, in contrast to Chronos as empirical time divided into past, present, and future. ... In Latin the concept of the deity may appear as Aevum or Saeculum.
-          <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
+        <CardActionArea>
+          <img src={mainImage} className={classes.media} alt="time-lapse"/>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Lizard
+            </Typography>
+            <Typography component="p">
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+              across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
       </Card>
     );
   }
