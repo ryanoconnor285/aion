b21ba0cc31f4f4b7300a6045baa3be1cd73e2dc0
Ryan O'Connor <rocsteady888@gmail.com>
2019-03-29 17:33:24 -0400

    added cards with pertinent hpotos

--- client/src/components/layout/content/Landing.js
+++ client/src/components/layout/content/Landing.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { withStyles } from '@material-ui/core/styles';
-import MediaCard from '../../common/MediaCard';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+import LandingAppBar from '../navigation/LandingAppBar';
 import mainImage from '../../../images/action-asphalt-back-light-315938.jpg';
-import infoCard1 from '../../../images/abstract-astronomy-dark-924824.jpg';
+import infoCard1 from '../../../images/adult-agreement-beard-541522.jpg';
 import infoCard2 from '../../../images/asphalt-blur-car-290470.jpg';
 import infoCard3 from '../../../images/action-blur-city-590701.jpg';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
@@ -19,9 +19,22 @@ const styles = theme => ({
   mainCard: {
     width: '100%',
   },
+  mainCardP: {
+    margin: 'auto',
+    width: '50%',
+  },
+  mainCardActions: {
+    margin: 'auto',
+  },
   mainMedia: {
     marginTop: -50,
-    width: '110%',
+    width: '100%',
+  },
+  card: {
+    maxWidth: '375',
+  },
+  media: {
+    height: 150,
   },
 });
 
@@ -37,57 +50,83 @@ class Landing extends React.Component {
     return (
       <div>
         <div className={classes.root}>
+        <LandingAppBar />
         <Grid container spacing={24}>
           <Grid item xs={12}>
             <Card className={classes.mainCard}>
-              <CardActionArea>
-                <img src={mainImage} className={classes.mainMedia} alt="time-lapse"/>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Lizard
-                  </Typography>
-                  <Typography component="p">
-                    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Share
-                </Button>
-                <Button size="small" color="primary">
-                  Learn More
-                </Button>
+              <img src={mainImage} className={classes.mainMedia} alt="time-lapse"/>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Aion
+                </Typography>
+                <Typography className={classes.mainCardP} component="p">
+                  Clock in, clock out, how hard could it be?  Aion can help you keep track of the amount of time you work even if your employer is using another system to track time.  Your record  is easy to use and easy to access anywhere and anytime, mobile or desktop.  
+                </Typography>
+              </CardContent>
+              <CardActions className={classes.mainCardActions}>
+                <Button color="inherit" component={Link} to="/register">Register</Button>
               </CardActions>
             </Card>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <MediaCard 
-              image={infoCard1}
-              title={"ClockInTracking"}
-              headline={"Headline"}
-              info={"Short parahgraph describing the benefits"}
-              cardActionText={"Button Text"}
-            />
+            <Card className={classes.card}>
+              <img
+                className={classes.media}
+                src={infoCard1}
+                alt="time-lapse"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Mobile Friendly
+                </Typography>
+                <Typography component="p">
+                  This app is designed to be used on mobile to help you quickly keep track of your time from anywhere.  It also works great on desktop with easy to read tables.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button color="inherit" component={Link} to="/register">Register</Button>
+              </CardActions>
+            </Card>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <MediaCard 
-              image={infoCard2}
-              title={"ClockInTracking"}
-              headline={"Headline"}
-              info={"Short parahgraph describing the benefits"}
-              cardActionText={"Button Text"}
-            />
+            <Card className={classes.card}>
+              <img
+                className={classes.media}
+                src={infoCard2}
+                alt="time-lapse"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Intuitive
+                </Typography>
+                <Typography component="p">
+                  This app intuitively presents you with the information and actions you need to make tracking easy.  When you are clocked in, for example, you will be prompted to clock out the next time you use the app.  
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button color="inherit" component={Link} to="/register">Register</Button>
+              </CardActions>
+            </Card>
           </Grid>
           <Grid item xs={12} sm={4}>
-            <MediaCard 
-              image={infoCard3}
-              title={"ClockInTracking"}
-              headline={"Headline"}
-              info={"Short parahgraph describing the benefits"}
-              cardActionText={"Button Text"}
-            />
+            <Card className={classes.card}>
+              <img
+                className={classes.media}
+                src={infoCard3}
+                alt="time-lapse"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Built Around You
+                </Typography>
+                <Typography component="p">
+                  Let Aion make time tracking simple and easy for you.  You can quickly record shifts, billable hours, or even time spent on a project.  If you work 8, 12 or even 24 hours Aion can easily help you see .  
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button color="inherit" component={Link} to="/register">Register</Button>
+              </CardActions>
+            </Card>
           </Grid>
         </Grid>
       </div>
