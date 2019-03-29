2ad4dc1634f82faae7ac7182922375c1a190c3f4
Ryan O'Connor <rocsteady888@gmail.com>
2019-03-29 17:33:31 -0400

    intiial commit

---
+++ client/src/components/layout/navigation/LandingAppBar.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../../actions/authActions';
+import ClockInModal from '../content/shifts/ClockInModal';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const styles = {
+  navbar: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class Navbar extends React.Component {
+
+  onLogoutClick = (e) => {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { isAuthenticated } = this.props.auth;
+
+    const authLinks = (
+      <div>
+        <Button color="inherit" onClick={this.onLogoutClick}>Logout</Button>
+      </div>
+    );
+
+    const guestLinks = (
+      <div>
+        <Button color="inherit" component={Link} to="/login">Login</Button>
+        <Button color="inherit" component={Link} to="/register">Register</Button>
+      </div>
+    );
+
+    return (
+      <div className={classes.navbar}>
+        <AppBar position="fixed" color="default">
+          <Toolbar>
+            {isAuthenticated ? <ClockInModal /> : null}
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Aion
+            </Typography>
+            {isAuthenticated ? authLinks : guestLinks}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+
+export default connect(mapStateToProps, { logoutUser })(withStyles(styles)(Navbar));
